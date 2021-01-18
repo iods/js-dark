fab67ad25fc8c03fa8e4f8188fdb2a8785354ba6
Rye Miller <millerrye17@gmail.com>
2021-01-18 03:20:14 -0700

    Addition of application and exercise library for projects.

---
+++ Library/exercises/calculator/assets/scripts/app.js
@@ -0,0 +1,9 @@
+const defaultResult = 0
+
+let currentResult = defaultResult
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(' + currentResult + '10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription)
