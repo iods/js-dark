80a003fd8b62b28a05baaea336e6ae4eac9ee92e
Rye Miller <millerrye17@gmail.com>
2021-01-26 20:11:07 -0700

    Addition of Vue project and environment for development.

---
+++ Library/developer-vue/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            fontSize: {
+                large: '200px'
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+};
