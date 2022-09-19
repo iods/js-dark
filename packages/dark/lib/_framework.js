3d913f40e7e77e95b5d2cb8dff1cb70b26dee1a2
Rye Miller <millerrye17@gmail.com>
2022-09-18 23:50:23 -0600

    #2: Update to the base package and framework defaults.

---
+++ packages/dark/lib/_framework.js
@@ -0,0 +1,9 @@
+/*
+ * DarkJS
+ *
+ * A lightweight, full-stack JavaScript framework for keeping the lights on.
+ */
+
+const Dark = require ('./dark');
+
+module.exports = Dark.version;
---
+++ packages/dark/lib/dark.js
@@ -0,0 +1,10 @@
+/*
+ * DarkJS
+ *
+ * A lightweight, full-stack JavaScript framework for keeping the lights on.
+ */
+
+module.exports = exports = {
+    version: "0.2.0"
+}
+
