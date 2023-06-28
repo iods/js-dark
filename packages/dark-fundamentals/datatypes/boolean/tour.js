09e1e52ad73890a5663054ceb8c07efd1e9fdb98
Rye Miller <millerrye17@gmail.com>
2023-06-28 15:31:41 -0600

    Addition of a boolean datatype package.

---
+++ packages/dark-fundamentals/datatypes/boolean/tour.js
@@ -0,0 +1,12 @@
+/*
+ * DarkJSDK
+ * A Tour of Booleans
+ */
+
+// JavaScript supports booleans, true or false values:
+
+let isSunny = true;
+let isRaining = false;
+
+console.log(isSunny);   // returns true
+console.log(isRaining); // returns false
