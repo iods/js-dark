061121b9531f7f8e9e84dd834600d86f21e4e7ab
Rye Miller <millerrye17@gmail.com>
2023-06-29 22:04:52 -0600

    #38: Addition of the Date object with examples.

---
+++ packages/dark-fundamentals/dates/methods.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Date Methods
+ * Provides an understanding of Dates for the DarkJSDK
+ * @package
+ */
+
+const date = new Date();
+
+console.log(date.getSeconds());
+console.log(date.getMinutes());
+console.log(date.getHours());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getDate());
+console.log(date.getTime());
