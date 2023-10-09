2b2f6609796efae559313a367f6a280a1ea5e771
Rye Miller <millerrye17@gmail.com>
2023-10-09 13:04:56 -0600

    Addition of Footer component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/index.js';
+import Header from './components/Header';
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Header />
-  );
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
